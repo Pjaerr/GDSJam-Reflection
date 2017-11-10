5a1467d42f5c4a63b51f4f12c0dc9cc805b2da50
Joshua Jackson <washingtonfaulconer@gmail.com>
2017-11-10 19:46:22 +0000

    Initial Commit
    Has some turning mirrors and stuff

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	Transform trans; //Cached reference to this object's transform.
+	[SerializeField] float movementSpeed; //Movement Speed, adjustable in the editor.
+
+	void Start()
+	{
+		trans = GetComponent<Transform>(); //Stores reference to this object's transform.
+	}
+
+	//Update() is called every frame.
+	void Update()
+	{
+		Movement();
+	}
+	
+
+	void Movement()
+	{
+		float step = movementSpeed * Time.deltaTime; //Stores movementspeed * deltatime to keep movement non-fps dependant.
+
+		/*Checks for WASD, and increases or decreases x/y by 1, and then translates this player by those amounts
+		multiplied by the step created above.*/
+		
+		float x = 0;
+		float y = 0;
+
+		if (Input.GetKey(KeyCode.W))
+		{
+			y += 1;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			y -= 1;
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			x -= 1;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			x += 1;
+		}
+
+		trans.Translate(new Vector3(x * step, y * step));
+	}
+	
+}
