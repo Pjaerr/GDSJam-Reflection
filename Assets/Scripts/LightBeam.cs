679d8d98c2582d4d775aa87d7cbe5886c97721df
Joshua Jackson <washingtonfaulconer@gmail.com>
2017-11-11 15:28:28 +0000

    Reflections Working
    Game jam over pretty much

--- Assets/Scripts/LightBeam.cs
+++ Assets/Scripts/LightBeam.cs
@@ -13,7 +13,7 @@ public class LightBeam : MonoBehaviour
 	{
 		trans = GetComponent<Transform>();
 		lineRenderer = GetComponent<LineRenderer>();
-		lineRenderer.positionCount = 3;
+		lineRenderer.positionCount = 2;
 
 		playerScript = GameManager.instance.Player.GetComponent<PlayerController>();
 	}
@@ -22,46 +22,52 @@ public class LightBeam : MonoBehaviour
 
 	void Update()
 	{
+		previousHitPoint = trans.position;
 		lineNumber = 0;
-		TheReflectionDetectionFunction();
+		lineRenderer.positionCount = 2;
+		TheReflectionDetectionFunction(trans.right);
 	}
 
-	void TheReflectionDetectionFunction()
+	Vector3 previousHitPoint;
+
+	void TheReflectionDetectionFunction(Vector3 direction)
 	{
+		
 		//Sets the start position of our laser to the initial objects position
-		lineRenderer.SetPosition(lineNumber, trans.position);
+		lineRenderer.SetPosition(lineNumber, previousHitPoint);
 
 		//Fires a raycast from start positon to the right.
 		RaycastHit hit;
-		if (Physics.Raycast(trans.position, trans.right, out hit))
+		if (Physics.Raycast(previousHitPoint, direction, out hit))
 		{
 			lineNumber++;
-			/*Stops the raycast (which controls where the line goes) when it collides with
-			a gameobject tagged as "Obstacle".*/
-			if (hit.collider.tag == "Obstacle")
-			{
-				lineRenderer.SetPosition(lineNumber, hit.point);
 
-				while (lineNumber < lineRenderer.positionCount)
+			if (lineNumber < 10)
+			{
+				/*Stops the raycast (which controls where the line goes) when it collides with
+				a gameobject tagged as "Obstacle".*/
+				if (hit.collider.tag == "Obstacle")
 				{
-					lineNumber++;
 					lineRenderer.SetPosition(lineNumber, hit.point);
 				}
+
+				if (hit.collider.tag == "Mirror")
+				{
+					lineRenderer.positionCount += 1;
+					lineRenderer.SetPosition(lineNumber, hit.point);
+
+					Vector3 pos = Vector3.Reflect(hit.point - previousHitPoint, hit.normal) * maxDistance + hit.point;
+					lineRenderer.SetPosition(lineNumber + 1, pos);
+					previousHitPoint = hit.point;
+
+					TheReflectionDetectionFunction(pos);
+				}
 			}
-
-			if (hit.collider.tag == "Mirror")
-			{
-				lineRenderer.SetPosition(1, hit.point);
-
-				Vector3 pos = Vector3.Reflect(hit.point - trans.position, hit.normal) * maxDistance + hit.point;
-				lineRenderer.SetPosition(2, pos);
-
-				TheReflectionDetectionFunction();
-			}
+			
 		}
 		else
 		{
-			lineRenderer.SetPosition(lineNumber, hit.point * maxDistance);
+			lineRenderer.SetPosition(lineNumber, previousHitPoint * maxDistance);
 		}
 	}
 }
