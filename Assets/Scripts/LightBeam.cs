2f3962066988a3b0fe74146a06f4759ebef55f5c
Joshua Jackson <washingtonfaulconer@gmail.com>
2017-11-11 12:26:15 +0000

    Messed with relfections

--- Assets/Scripts/LightBeam.cs
+++ Assets/Scripts/LightBeam.cs
@@ -15,8 +15,10 @@ public class LightBeam : MonoBehaviour
 		lineRenderer = GetComponent<LineRenderer>();
 
 		playerScript = GameManager.instance.Player.GetComponent<PlayerController>();
+		Vector3 direction = trans.right;
 	}
 
+	
 	void Update()
 	{
 		lineRenderer.SetPosition(0, trans.position);
@@ -24,7 +26,46 @@ public class LightBeam : MonoBehaviour
 		RaycastHit hit;
 		if (Physics.Raycast(trans.position, trans.right, out hit))
 		{
+			
+			if (hit.collider.tag == "Mirror")
+			{
+				lineRenderer.SetPosition(1, hit.point);
+
+				Reflect(trans.position, hit.point, hit.collider.transform.rotation);
+			}
+			if (hit.collider.tag == "Obstacle")
+			{
+				lineRenderer.SetPosition(1, hit.point);
+			}
+			if (hit.collider.tag == "Player")
+			{
+				playerScript.KillPlayer();
+			}
+
 			if (hit.collider)
+			{
+				lineRenderer.SetPosition(1, hit.point);
+			}
+		}
+		else
+		{
+			lineRenderer.SetPosition(1, trans.right * maxDistance);
+		}
+	}
+
+	void Reflect(Vector3 startPoint, Vector3 endPoint, Quaternion rotation)
+	{
+		Debug.Log("EndPoint: " + endPoint + " , Rotation: " + rotation.eulerAngles);
+
+		RaycastHit hit;
+		if (Physics.Raycast(endPoint, new Vector3(-endPoint.x, -endPoint.y + 10, -endPoint.z), out hit))
+		{
+			if (hit.collider.tag == "Mirror")
+			{
+				lineRenderer.SetPosition(1, hit.point);
+				Reflect(hit.point, hit.collider.transform.rotation);
+			}
+			if (hit.collider.tag == "Obstacle")
 			{
 				lineRenderer.SetPosition(1, hit.point);
 			}
@@ -35,8 +76,7 @@ public class LightBeam : MonoBehaviour
 		}
 		else
 		{
-			lineRenderer.SetPosition(1, transform.right * maxDistance);
+			lineRenderer.SetPosition(1, direction * maxDistance);
 		}
-
 	}
 }
